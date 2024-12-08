7322445d01df9ee60e321ba0fe6cd24f4c973e5c
ajskateboarder <me@ajskateboarder.org>
2024-12-08 13:18:20 -0500

    update deployment

--- deploy/run.sh
+++ deploy/run.sh
@@ -1,27 +1,7 @@
-# assumes script is running in regular shell in cwd
-# please do so
-mkdir api
-
-# get the latest release tag
-tag=`gh release list -R hatchdotlol/hatch-api --json name | jq -r ".[0].name"`
-
-# if the latest exe exists and its not running, run it or skadoodle
-if [ $(ls api | grep "$tag") != "" ]; then
-        if [ "$(ps | grep api)" = "" ]; then
-          ./api/api* &
-        fi 
-        exit 0
+if [ "$(git pull | grep -i Already)" = "Already up to date." ]; then
+   exit 0;
 fi
 
-rm -rf api/*
-cd api
-
-# download latest release
-gh release download "$tag" -p "hatch-api_*_x86_64-unknown-linux-musl.zip" --clobber -R hatchdotlol/api
-unzip *_x86_64-unknown-linux-musl.zip
-
-mv ./api "./api_$tag"
-rm *.zip
-pkill api*
-
-./api* &
+pkill api
+cargo build --release
+./target/release/api &
