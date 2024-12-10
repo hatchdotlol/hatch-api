2d8d33baf26eda82aa9bb480136e908b34bfd2f5
ajskateboarder <me@ajskateboarder.org>
2024-12-09 21:23:35 -0500

    support cors on hatch.lol

--- README.md
+++ README.md
@@ -1,3 +1,40 @@
 # hatch API
 
-this is the repository for the API used by hatch.
+This is the repository for the API used by hatch.lol.
+
+## Running
+
+- Install the Rust toolchain, [Minio](https://min.io/docs/minio/linux/operations/installation.html), and SQLite drivers (on Debian this is `libsqlite-dev` if you don't have it by default)
+- Start a Minio server in the background bound to `localhost:9000`
+  - Make a bucket named "pfps" and one named "assets"
+- `cargo run`
+
+This API has only been tested on Linux.
+
+## Example use
+
+```py
+import requests
+
+BASE = "https://api.hatch.lol"
+
+# logging in
+login = requests.post(
+    f"{BASE}/auth/login",
+    json={"username": "...", "password": "..."}
+)
+token = login.json()["token"]
+
+# testing the token
+me = requests.get(f"{BASE}/auth/me", headers={"token": token})
+print(me.json())
+# {
+#     'user': '...',
+#     'displayName': '...',
+#     'country': '...',
+#     'bio': '...',
+#     'highlightedProjects': '...',
+#     'profilePicture': '...',
+#     'joinDate': '...'
+# }
+```
--- src/main.rs
+++ src/main.rs
@@ -7,8 +7,9 @@ pub mod routes;
 pub mod structs;
 pub mod token_header;
 
-use rocket::http::Status;
+use rocket::http::{Method, Status};
 use rocket::response::{content, status};
+use rocket_cors::{AllowedHeaders, AllowedOrigins, CorsOptions};
 use routes::{auth, root, uploads, users};
 
 #[catch(404)]
@@ -31,10 +32,24 @@ fn bad_request() -> status::Custom<content::RawJson<String>> {
 
 #[launch]
 fn rocket() -> _ {
+    let allowed_origins = AllowedOrigins::some_exact(&["https://hatch.lol"]);
+
+    // You can also deserialize this
+    let cors = CorsOptions {
+        allowed_origins,
+        allowed_methods: vec![Method::Get].into_iter().map(From::from).collect(),
+        allowed_headers: AllowedHeaders::some(&["Authorization", "Accept"]),
+        allow_credentials: true,
+        ..Default::default()
+    }
+    .to_cors()
+    .unwrap();
+
     rocket::build()
         .mount("/", routes![root::comic_sans, root::index])
         .register("/", catchers![not_found, bad_request])
         .mount("/uploads", routes![uploads::update_pfp, uploads::user])
         .mount("/auth", routes![auth::login, auth::logout, auth::me])
         .mount("/user", routes![users::user, users::update_user_info])
+        .attach(cors)
 }
