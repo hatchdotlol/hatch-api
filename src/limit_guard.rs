028b78688d26be0a44df4e9466c93c7fb44b9899
ajskateboarder <me@ajskateboarder.org>
2025-03-01 21:17:30 -0500

    10 per second is actually 10 per second

--- src/limit_guard.rs
+++ src/limit_guard.rs
@@ -4,13 +4,13 @@ pub struct OnePerMinute;
 
 impl<'r> RocketGovernable<'r> for OnePerMinute {
     fn quota(_method: Method, _route_name: &str) -> Quota {
-        Quota::per_minute(Self::nonzero(1u32))
+        Quota::per_minute(Self::nonzero(1))
     }
 }
 pub struct TenPerSecond;
 
 impl<'r> RocketGovernable<'r> for TenPerSecond {
     fn quota(_method: Method, _route_name: &str) -> Quota {
-        Quota::per_minute(Self::nonzero(1u32))
+        Quota::per_second(Self::nonzero(10))
     }
 }
