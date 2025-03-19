5325b4300b29432854ad86a0afea1cb9de0df202
ajskateboarder <me@ajskateboarder.org>
2025-03-18 20:30:24 -0400

    i misunderstood the basic reqs of voting

--- src/db/wrapper.rs
+++ src/db/wrapper.rs
@@ -317,10 +317,6 @@ impl SqliteBackend {
                 "DELETE FROM votes WHERE user = ?1 AND project = ?2 AND type = ?3",
                 (user, id, upvote),
             )?;
-            self.client.execute(
-                "INSERT INTO votes (user, project, type) VALUES (?1, ?2, ?3)",
-                (user, id, set_upvote),
-            )?;
         }
 
         Ok(())
