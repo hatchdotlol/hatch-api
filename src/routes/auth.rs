a86c3468218ec1db211ab5178f7cb4f4bf3a4c7f
ajskateboarder <me@ajskateboarder.org>
2025-03-18 23:47:10 -0400

    make email verification on integer usernames doable
    this will probably also cause crashes elsewhere so i might also not allow number usernames

--- src/routes/auth.rs
+++ src/routes/auth.rs
@@ -435,7 +435,7 @@ pub fn verify(email_token: &str) -> Redirect {
         .unwrap();
 
     let _ = select.query_row((email_token,), |r| {
-        let user: String = r.get(1).unwrap();
+        let user = r.get::<usize, String>(1).unwrap_or(r.get::<usize, u32>(1).unwrap().to_string());
         if r.get::<usize, i64>(3).unwrap() >= chrono::Utc::now().timestamp() {
             cur.client
                 .execute("UPDATE users SET verified=TRUE WHERE name= ?1", (&user,))
