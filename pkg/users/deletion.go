efc8664b97b6cdee966e1e813cbb9a69ec767881
ajskateboarder <me@ajskateboarder.org>
2025-07-05 12:22:25 -0400

    more deletion logic

--- pkg/users/deletion.go
+++ pkg/users/deletion.go
@@ -1,45 +1,82 @@
 package users
 
 import (
-	"fmt"
-	"strings"
+	"context"
 
+	"github.com/getsentry/sentry-go"
 	"github.com/hatchdotlol/hatch-api/pkg/db"
+	"github.com/minio/minio-go/v7"
 )
 
-func in(baseQuery string, values any) string {
-	numValues := len(values.([]any))
-
-	placeholders := make([]string, numValues)
-	for i := range placeholders {
-		placeholders[i] = "?"
-	}
-	inClause := strings.Join(placeholders, ",")
-
-	modifiedQuery := strings.Replace(baseQuery, "(?)", fmt.Sprintf("(%s)", inClause), 1)
-
-	return modifiedQuery
-}
-
 func ScheduleDeletion(user int64) error {
-	rows, err := db.Db.Query("SELECT id FROM uploads WHERE uploads = ?", user)
+	rows, err := db.Db.Query("SELECT id, buckets FROM uploads WHERE uploads = ?", user)
 	if err != nil {
 		return err
 	}
 	defer rows.Close()
 
-	uploads := []int64{}
+	uploadIds := []string{}
+	uploadBuckets := []string{}
 
 	for rows.Next() {
-		var id int64
-		if err := rows.Scan(&id); err != nil {
+		var id string
+		var bucket string
+		if err := rows.Scan(&id, &bucket); err != nil {
 			return err
 		}
-		uploads = append(uploads, id)
+		uploadIds = append(uploadIds, id)
+		uploadBuckets = append(uploadBuckets, bucket)
 	}
 
-	query := in("DELETE FROM uploads WHERE uploader IN (?)", uploads)
-	db.Db.Exec(query, uploads)
+	if _, err := db.Db.Exec("DELETE FROM uploads WHERE uploader = ?", user); err != nil {
+		return err
+	}
+
+	// collect all user objects into channels
+	projectCh := make(chan minio.ObjectInfo)
+	thumbnailCh := make(chan minio.ObjectInfo)
+	pfpCh := make(chan minio.ObjectInfo)
+
+	go func() {
+		defer close(projectCh)
+		defer close(thumbnailCh)
+		defer close(pfpCh)
+
+		for i := range uploadIds {
+			object, err := db.Uploads.GetObject(context.Background(), uploadBuckets[i], uploadIds[i], minio.GetObjectOptions{})
+			if err != nil {
+				sentry.CaptureException(err)
+				continue
+			}
+			info, err := object.Stat()
+			if err != nil {
+				sentry.CaptureException(err)
+				continue
+			}
+			switch uploadBuckets[i] {
+			case "pfps":
+				pfpCh <- info
+			case "projects":
+				projectCh <- info
+			case "thumbnails":
+				thumbnailCh <- info
+			}
+		}
+	}()
+
+	opts := minio.RemoveObjectsOptions{
+		GovernanceBypass: true,
+	}
+
+	for rErr := range db.Uploads.RemoveObjects(context.Background(), "projects", projectCh, opts) {
+		sentry.CaptureException(&rErr)
+	}
+	for rErr := range db.Uploads.RemoveObjects(context.Background(), "thumbnails", thumbnailCh, opts) {
+		sentry.CaptureException(&rErr)
+	}
+	for rErr := range db.Uploads.RemoveObjects(context.Background(), "pfps", pfpCh, opts) {
+		sentry.CaptureException(&rErr)
+	}
 
 	return nil
 }
